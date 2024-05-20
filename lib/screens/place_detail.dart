95dec567d20263229597bcd23fbd33196bc97002
KhalilGr <garalikhalil@gmail.com>
2024-05-20 00:54:16 -0400

    Add Project

--- lib/screens/place_detail.dart
+++ lib/screens/place_detail.dart
@@ -11,7 +11,7 @@ class PlaceDetailScreen extends StatelessWidget {
   String get locationImage {
     final lat = place.location.latitude;
     final lng = place.location.longitude;
-    return 'https://maps.googleapis.com/maps/api/staticmap?center=$lat,$lng=&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$lat,$lng&key=AIzaSyDm6PXLdZGF-Jzrr7195akfNR5_VDHCAzE';
+    return 'https://maps.googleapis.com/maps/api/staticmap?center=$lat,$lng=&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$lat,$lng&key=YOURKEYHERE';
   }
 
   @override
--- lib/widgets/location_input.dart
+++ lib/widgets/location_input.dart
@@ -27,7 +27,7 @@ class _LocationInputState extends State<LocationInput> {
     }
     final lat = _pickedLocation!.latitude;
     final lng = _pickedLocation!.longitude;
-    return 'https://maps.googleapis.com/maps/api/staticmap?center=$lat,$lng=&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$lat,$lng&key=AIzaSyDm6PXLdZGF-Jzrr7195akfNR5_VDHCAzE';
+    return 'https://maps.googleapis.com/maps/api/staticmap?center=$lat,$lng=&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$lat,$lng&key=YOURKEYHERE';
   }
 
   Future<void> _savePlace(double latitude, double longitude) async {
